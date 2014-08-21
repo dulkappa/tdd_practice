9ec8bbb24880dfcd3c06f50be0599930f4f6c319
dulkappa <dulkappa@gmail.com>
2014-08-21 22:33:52 +0900

    Modify FizzBuzz.py to pass the case number is 5

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz(num):
-  if num == 3:
+  if num%3 == 0:
     return "Fizz"
+  elif num%5 == 0:
+    return "Buzz"
   else:
     return num
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -9,5 +9,8 @@ class FizzBuzzTest(unittest.TestCase):
   def test_fizzbuzz_get_3(self):
     self.assertEqual(fizzbuzz(3), "Fizz")
 
+  def test_fizzbuzz_get_5(self):
+    self.assertEqual(fizzbuzz(5), "Buzz")
+
 if __name__ == "__main__":
   unittest.main()
